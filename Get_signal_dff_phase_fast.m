a4696d3ba5f8164a2e2b23baa9eb5f050ea2bbb9
Geoffrey <migault@hotmail.fr>
2018-03-14 10:47:33 +0100

    Plot improvement

--- Get_signal_dff_phase_fast.m
+++ Get_signal_dff_phase_fast.m
@@ -12,6 +12,11 @@ nframes = size(F.set.frames, 2);    % number of frames in one layer
 f = 1/F.dt * 1000;                  % image acquisition frequency (at which the camera runs)
 Time = [(1/f)*layer:(1/f)*nlayer:(nframes*(1/f)*nlayer)];             % time of experiment sampled at image aquisition frequency
 Time_stim = [0:(1/f)*nlayer:(nframes*(1/f)*nlayer)-((1/f)*nlayer)];             % time of experiment sampled at image aquisition frequency
+a = [1:3:N;(1:3:N)+1];
+Lin_first = size(a,2)+1;
+Lin = Lin_first+N;
+Col = 3;
+%
 
 pstim = size(Time, 2) * F.dt*0.001 * length(F.sets) * fstim;
 if mod(nframes/pstim,1)==0
@@ -21,27 +26,50 @@ else
 end
 
 % =========== Load phasestack image
-im_PhaseMap = imread([ F.Files 'Phase_map_normalized/PhaseMap_DFF_pix_fstim', num2str(fstim),'/Images/Images0_' num2str(layer,'%02d') '.tif']);
-ax(1) = subplot(3,3,1);
-h_im = imshow(rescalegd2(im_PhaseMap));
-hold(ax(1), 'on');
+try
+    im_PhaseMap = imread([ F.Files 'Phase_map_normalized/PhaseMap_DFF_pix_fstim', num2str(fstim),'/Images/Images0_' num2str(layer,'%02d') '.tif']);
+    if N > 3
+        ax(1) = subplot(Lin,Col,reshape(a,1,[]));
+    else
+        ax(1) = subplot(Lin,Col,[1:2,4:5]);
+    end
+    imshow(rescalegd2(im_PhaseMap));
+    axis off
+    hold(ax(1), 'on');
+catch
+    warning('Phase stack doesn t exit (gray stack default)');
+    im_GreyStack = imread([ F.Files 'grey_stack/Image_' num2str(layer,'%02d') '.tif']);
+    if N > 3
+        ax(1) = subplot(Lin,Col,reshape(a,1,[]));
+    else
+        ax(1) = subplot(Lin,Col,[1:2,4:5]);
+    end
+    imshow(rescalegd2(im_GreyStack));
+    axis off
+    hold(ax(1), 'on');
+end
 
 % =========== Load greystack image
 im_GreyStack = imread([ F.Files 'grey_stack/Image_' num2str(layer,'%02d') '.tif']);
-ax(2) = subplot(3,3,2);
+if N > 3
+    ax(2) = subplot(Lin,Col,(1:3:N)+2);
+else
+    ax(2) = subplot(Lin,Col,[3,6]);
+end
 h_im = imshow(rescalegd2(im_GreyStack));
+axis off
 hold(ax(2), 'on');
 
 % =========== Load Tracking's trace
 try
     [Tracking_sig, Tracking_time] = tracking_signal(F);
-    ax(3) = subplot(3,3,7:8);
+    ax(3) = subplot(Lin,Col,Lin_first*Col-2:Lin_first*Col-1);
     hold(ax(3), 'on');
     plot(Tracking_time, -Tracking_sig, 'r');
     [Tracking_int] = tracking_interpolate(Time_stim, F);
     Tracking_int(Tracking_int==0) = nan;
     Tracking_sig_mean = nanmean(reshape(Tracking_int,[size(Tracking_int,2)/pstim, pstim]),2);
-    ax(6) = subplot(3,3,9);
+    ax(6) = subplot(Lin,Col,Lin_first*Col);
     hold(ax(6), 'on');
     plot(Time_stim(1:size(Tracking_sig_mean,1)), -(Tracking_sig_mean-min(Tracking_sig_mean)) , 'r');
 catch
@@ -51,18 +79,14 @@ end
 % =========== Load Motor's trace
 try
     [Motor_sig, Motor_time] = motor_signal(F);
-    ax(3) = subplot(3,3,7:8);
-    plot(Motor_time, Motor_sig, 'b');
+    ax(3) = subplot(Lin,Col,Lin_first*Col-2:Lin_first*Col-1);
+    plot(Motor_time, Motor_sig, 'k');
     axis([0 600 -20 20]);
     [Motor_int] = motor_interpolate(Time_stim, F);
     Motor_sig_mean = reshape(Motor_int,[size(Motor_int,2)/pstim, pstim]);
     Motor_sig_mean = Motor_sig_mean(:,2);
-    ax(6) = subplot(3,3,9);
-    plot(Time_stim(1:size(Motor_sig_mean,1)), Motor_sig_mean, 'b');
-    ax(5) = subplot(3,3,6);
-    hold(ax(5), 'on');
-    yyaxis right
-    plot(Time_stim(1:size(Motor_sig_mean,1)), Motor_sig_mean, '-r');
+    ax(6) = subplot(Lin,Col,Lin_first*Col);
+    plot(Time_stim(1:size(Motor_sig_mean,1)), Motor_sig_mean, 'k');
 catch
     warning('No Motor trace');
 end
@@ -77,14 +101,14 @@ index = S.index;
 % delete(ROI);                             % delete the ROI overlay from the image
 % ind_ROI = find(mask_ROI);                % get indices of pixels inside the ROI
 % [~,ind_ROI] = intersect(index,ind_ROI);  % keep only indices of ROI-pixels that are inside the brain contour
-% 
+%
 % tmp_dff = zeros(size(ind_ROI(:),1), nframes);
 % for i = 1:size(ind_ROI(:),1)
-%     tmp_dff(i,:) = S.DFF_pix(ind_ROI(i,:),:);   
+%     tmp_dff(i,:) = S.DFF_pix(ind_ROI(i,:),:);
 % end
 % mean_Data = nanmean(tmp_dff);
 % clear tmp_dff
-% 
+%
 % ax(4) = subplot(3,3,4:5);
 % hold(ax(4), 'on');
 % plot(Time, mean_Data, '.') % Plot DFF of ROI (first point is shifted to zero)
@@ -120,12 +144,22 @@ while count <= N
     else
         Data_ROI_mean = mean(reshape(Data_ROI,[size(Data_ROI,2)/pstim, pstim]),2);
     end
-    ax(4) = subplot(3,3,4:5);
+    ax(4) = subplot(Lin,Col,(Col*Lin_first+((count-1)*3)+1):(Col*Lin_first+((count-1)*3)+2));
     plot(Time(1:size(Data_ROI,2)), Data_ROI,'color',colours(count,:),'LineWidth',1) % Plot DFF of ROI (first point is shifted to zero)
-    ax(5) = subplot(3,3,6);
-    hold(ax(5), 'on');
+    ax(5) = subplot(Lin,Col,(Col*Lin_first+((count-1)*3)+3));
     yyaxis left
     plot(Time(1:size(Data_ROI_mean,1)),Data_ROI_mean, 'color',colours(count,:),'LineWidth',1)
+    set(ax(5),'ycolor',colours(count,:))
+    yyaxis right
+    plot(Time_stim(1:size(Motor_sig_mean,1)), Motor_sig_mean, 'k');
+    set(ax(5),'ycolor','k')
+    
+    if count == N
+        ylabel('Tme [s]')
+    else
+        set(ax(5),'XTick',[])
+        set(ax(5),'XTick',[])
+    end
     
     % Save
     save_ROI{count, 1} = Data_ROI;
